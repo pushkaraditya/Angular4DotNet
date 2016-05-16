aa9d9a4dd37554d7630b3565716b084e02a761f8
Pushkar Aditya <pushkar.aditya@askiitians.com>
2016-05-16 16:41:00 +0530

    added packages

---
+++ Angular4DotNetMvc/Controllers/CoursesController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Angular4DotNetMvc.Controllers
+{
+  public class CoursesController : Controller
+  {
+    public ActionResult Index()
+    {
+      return View("Index", "", "you");
+    }
+  }
+}
