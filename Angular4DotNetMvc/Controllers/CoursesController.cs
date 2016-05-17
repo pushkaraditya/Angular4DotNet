57cd507f1d668b6578e967533bd7579c3c272184
Pushkar Aditya <pushkar.aditya@gmail.com>
2016-05-17 07:16:21 +0530

    Completed 3.5

--- Angular4DotNetMvc/Controllers/CoursesController.cs
+++ Angular4DotNetMvc/Controllers/CoursesController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Web.Mvc;
 
 namespace Angular4DotNetMvc.Controllers
 {
@@ -6,7 +8,25 @@ namespace Angular4DotNetMvc.Controllers
   {
     public ActionResult Index()
     {
-      return View("Index", "", "you");
+      return View("Index", "", GetSerializedCourseVms());
     }
+
+    public string GetSerializedCourseVms()
+    {
+      var courses = new [] {
+      new CourseVm { Number = "CREA101", Name = "Care of Magical Creatures", Instructor = "Rubeus Hagrid" },
+      new CourseVm { Number = "DARK501", Name = "Defense Against the Dark Arts", Instructor = "Severus Snape" },
+      new CourseVm { Number = "TRAN201", Name = "Transfiguration", Instructor = "Minerva McGonagall" }
+      };
+      var settings = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
+      return JsonConvert.SerializeObject(courses, settings);
+    }
+  }
+
+  public class CourseVm
+  {
+    public string Number { get; set; }
+    public string Name { get; set; }
+    public string Instructor { get; set; }
   }
 }
